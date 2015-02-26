d5889f759f04f7fb48885cefc61f93b71c48a23b
Matthew Darby <me@mjdarby.net>
2015-02-26 01:46:58 +0000

    Basically done, I guess! Should test it a bit.

--- brainnyan.py
+++ brainnyan.py
@@ -19,7 +19,7 @@ def read_keywords(json_string):
     return keyword_map
 
 def strip_non_keywords(keyword_map, text):
-    regex = "|".join(keyword_map.keys())
+    regex = "|".join([re.escape(x) for x in keyword_map.keys()])
     regex = "(" + regex + ")"
     split = re.split(regex, text)
     stripped = [x for x in split if x in keyword_map.keys()]
@@ -29,7 +29,32 @@ def replace_tokens(keyword_map, stripped):
     return "".join([keyword_map[x] for x in stripped])
 
 def interpret_bf(code):
+    numCells = 30000
+    data = [0] * numCells
+    instr_pointer = 0
+    data_pointer = 0
     parentheses = get_parentheses(code)
+    while instr_pointer < len(code):
+        instr = code[instr_pointer]
+        if instr == '>':
+            data_pointer += 1
+        elif instr == '<':
+            data_pointer -= 1
+        elif instr == '+':
+            data[data_pointer] += 1
+        elif instr == '-':
+            data[data_pointer] -= 1
+        elif instr == '.':
+            print(chr(data[data_pointer]), end='')
+        elif instr == ',':
+            data[data_pointer] = bytes(input(), 'utf-8')[0]
+        elif instr == '[':
+            if not data[data_pointer]:
+                instr_pointer = parentheses[instr_pointer]
+        elif instr == ']':
+            if data[data_pointer]:
+                instr_pointer = parentheses[instr_pointer]
+        instr_pointer += 1
 
 def run_code(keyword_map, text):
     stripped = strip_non_keywords(keyword_map, text)
@@ -37,7 +62,6 @@ def run_code(keyword_map, text):
     interpret_bf(code)
 
 if __name__ == "__main__":
-    print(sys.argv)
     if (len(sys.argv) < 2 or len(sys.argv) > 4 or
         (sys.argv[1] != "-i" and len(sys.argv) == 4)):
         print("Usage: ./brainnyan.py [-i] input.b > output.spl")
